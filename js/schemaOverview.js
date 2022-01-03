fca6e66b5f5fe6d06371107ef52ef1cb6f2e23c5
Ghislaine Guerin <me@ghislaineguerin.com>
2022-01-03 19:14:09 +0100

    add failure message

--- js/schemaOverview.js
+++ js/schemaOverview.js
@@ -20,7 +20,7 @@ export function schemaOverview(tables) {
                 <p class="text-sm">${tables.filter(t => t.type == 'table').length} Tables ${tables.filter(t => t.type == 'view').length} Views</p></div>
        </div>
   
-   <div>
+    <div>
         <h3 class="py-4 text-xl">Favorites</h3>
         ${tables.filter(t => t.favorite).slice(0,3).map( t => {
             return   `<div><a href="#" class="${theme.primaryTextColor}">${t.name}</a></div>`
@@ -35,10 +35,23 @@ export function schemaOverview(tables) {
         <div><a href="#" class="${theme.primaryTextColor}">Create a Table</a></div>
         <div><a href="#" class="${theme.primaryTextColor}">Create a View</a></div>
        
-   </div>
-</div>
-    
-    `;
+    </div>
+    </div>`;
+
+    if (activeSchema == 'loading_error_schema') {
+        schemaList.innerHTML = `<div class="${theme.textColor} ${theme.tableBorderColor}">
+       
+        <div class="flex items-center space-x-4 mb-4">
+            <div class="text-4xl text-center"><i class="ri-share-line align-middle"></i></div>
+            <div>
+                 <h2 class="text-xl font-semibold">${activeSchema}</h2>
+                 <p class="text-sm text-red-400"><i class="ri-error-warning-fill align-bottom text-red-400"></i> Failed to Load</div>
+        </div>
+   
+     <div>`
+    }
+
+
     appWrapper.querySelector('.table-wrapper').append(schemaList);
 }
 
